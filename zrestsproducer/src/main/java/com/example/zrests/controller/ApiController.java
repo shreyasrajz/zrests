bfc1d252db387d85b7744441f445a03c7d96d976
shreyasrajz <srigadha.raj@zomato.com>
2023-05-22 14:36:09 +0530

    synchronous requests

--- zrestsproducer/src/main/java/com/example/zrests/controller/ApiController.java
+++ zrestsproducer/src/main/java/com/example/zrests/controller/ApiController.java
@@ -4,13 +4,17 @@ import com.example.zrests.kafka.Producer;
 import com.example.zrests.model.Restaurant;
 import com.example.zrests.requests.Request2;
 import com.example.zrests.service.RestaurantService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @RestController
 public class ApiController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ApiController.class);
     private Producer producer;
     private RestaurantService restaurantService;
 
@@ -19,11 +23,14 @@ public class ApiController {
         this.restaurantService=restaurantService;
     }
     @PostMapping("/add")
-    public String add(@RequestBody Restaurant restaurant,@RequestHeader(value = "sync",defaultValue = "false") Boolean sync) {
+    public String add(@RequestBody Restaurant restaurant,
+                      @RequestHeader(value = "sync",defaultValue = "false") Boolean sync) {
         if(sync) {
-            System.out.println(restaurantService.save(restaurant));
-            System.out.println("A synchronous request is made");
-            return "sync ok";
+            LOGGER.info("Synchronous add request is made");
+            Restaurant saved = restaurantService.save(restaurant);
+            return "Synchronous Request \n"+
+                    saved.toString() +
+                    "\nis added to the database";
         }
         Request2 request = new Request2();
         request.setType("Add");
@@ -32,11 +39,14 @@ public class ApiController {
         return "ok";
     }
     @GetMapping(value = "/listall")
-    public String listAll(HttpServletRequest httpServletRequest,@RequestHeader(value = "sync",defaultValue = "false")Boolean sync) {
+    public String listAll(HttpServletRequest httpServletRequest,
+                          @RequestHeader(value = "sync",defaultValue = "false")Boolean sync) {
         if(sync) {
-            System.out.println(restaurantService.listAll());
-            System.out.println("A synchronous request is made");
-            return "sync ok";
+            List<Restaurant> restaurantList = restaurantService.listAll();
+            System.out.println(restaurantList);
+            return "Synchronous Request \n"+
+                    "List of restaurants: \n"+
+                    restaurantList.toString();
         }
         System.out.println(httpServletRequest.getHeaderNames().toString());
         Request2 request = new Request2();
@@ -46,11 +56,19 @@ public class ApiController {
         return "ok";
     }
     @GetMapping("/list/{id}")
-    public String listByID(@PathVariable("id") Long id,@RequestHeader(value = "sync",defaultValue = "false") Boolean sync) {
+    public String listByID(@PathVariable("id") Long id,
+                           @RequestHeader(value = "sync",defaultValue = "false") Boolean sync) {
         if(sync) {
-            System.out.println(restaurantService.listByID(id));
-            System.out.println("A synchronous request is made");
-            return "sync ok";
+            Restaurant restaurant = restaurantService.listByID(id);
+            if(restaurant.getId() == null) {
+                return String.format(
+                        "Synchronous Request \n"+
+                                "Restaurant with id: %s is not found",id);
+            }
+            return String.format(
+                    "Synchronous Request \n"+
+                    "Restaurant with id: {} \n",id) +
+                    restaurant.toString();
         }
         Request2 request = new Request2();
         request.setType("ListByID");
@@ -60,17 +78,42 @@ public class ApiController {
         return "ok";
     }
     @DeleteMapping("/delete/{id}")
-    public String deleteByID(@PathVariable("id") Long id,@RequestHeader(value = "sync",defaultValue = "false") Boolean sync) {
+    public String deleteByID(@PathVariable("id") Long id,
+                             @RequestHeader(value = "sync",defaultValue = "false") Boolean sync) {
         if(sync) {
-            System.out.println(restaurantService.deleteByID(id));
-            System.out.println("A synchronous request is made");
-            return "sync ok";
+            restaurantService.deleteByID(id);
+            return String.format(
+                    "Synchronous Request \n"+
+                            "Restaurant with id: %s is deleted",id);
         }
         Request2 request = new Request2();
         request.setType("DeleteByID");
         request.setId(id);
         producer.sendmessage(request);
         System.out.println(String.format("Request to delete restaurant with id: %s",id));
+        return "ok";
+    }
+    @PutMapping("/update/{id}")
+    public String updateByID(@PathVariable("id") Long id,
+                             @RequestHeader(value = "sync",defaultValue = "false") Boolean sync,
+                             @RequestBody Restaurant restaurant) {
+        if(sync) {
+            restaurant = restaurantService.updateById(id,restaurant);
+            if(restaurant.getId()==null) {
+                return "Synchronous Request \n"+
+                        "Restaurant not found";
+            }
+            return String.format(
+                    "Synchronous Request \n"+
+                            "Restaurant with id: %s is updated \n",id) +
+                    restaurant;
+        }
+        Request2 request = new Request2();
+        request.setType("UpdateByID");
+        request.setId(id);
+        request.setRestaurant(restaurant);
+        producer.sendmessage(request);
+        System.out.println(String.format("Request to Update restaurant with id: %s",id));
         return "ok";
     }
     @GetMapping("/hello")
--- zrestsproducer/src/main/java/com/example/zrests/service/RestaurantService.java
+++ zrestsproducer/src/main/java/com/example/zrests/service/RestaurantService.java
@@ -2,15 +2,27 @@ package com.example.zrests.service;
 
 import com.example.zrests.model.Restaurant;
 import com.example.zrests.repository.RestaurantRepo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPooled;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
 public class RestaurantService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(RestaurantService.class);
     private RestaurantRepo restaurantRepo;
+
+    JedisPooled jedis = new JedisPooled("localhost", 6379);
+    final Gson gson = new Gson();
 
     public RestaurantService(RestaurantRepo restaurantRepo) {
         this.restaurantRepo = restaurantRepo;
@@ -18,7 +30,12 @@ public class RestaurantService {
 
     public boolean deleteByID(Long id) {
         try {
+            String key = "restaurant:"+id.toString();
+            boolean idPresentInRedis = jedis.exists(key);
             restaurantRepo.deleteById(id);
+            if(idPresentInRedis) {
+                jedis.del(key);
+            }
             return true;
         } catch (Exception e) {
             System.out.println(e.toString());
@@ -26,19 +43,61 @@ public class RestaurantService {
         }
     }
 
-    public Optional<Restaurant> listByID(Long id) {
+    public Restaurant listByID(Long id) {
+        Restaurant restaurant;
         try {
-            return restaurantRepo.findById(id);
+            String key = "restaurant:"+id.toString();
+            boolean idPresentInRedis = jedis.exists(key);
+            if(idPresentInRedis) {
+                String value = jedis.get(key);
+                System.out.println(value);
+                ObjectMapper mapper = new ObjectMapper();
+                restaurant = mapper.readValue(value,Restaurant.class);
+            }else {
+                System.out.println(String.format("%s is not present in the redis",key));
+                Optional<Restaurant> restaurantOptional = restaurantRepo.findById(id);
+                if(restaurantOptional.isPresent()) {
+                    restaurant = restaurantOptional.get();
+                    jedis.set("restaurant:"+ id,gson.toJson(restaurant));
+                }else {
+                    restaurant = new Restaurant();
+                    LOGGER.info("Restaurant with given is not found");
+                }
+            }
+            return restaurant;
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
-
     public List<Restaurant> listAll() {
+        LOGGER.info("Listing all the restaurants");
         return restaurantRepo.findAll();
     }
 
     public Restaurant save(Restaurant restaurant) {
+        LOGGER.info("Adding new restaurant to database");
         return restaurantRepo.save(restaurant);
+    }
+
+    public Restaurant updateById(Long id, Restaurant newRestaurant) {
+        String key = "restaurant:"+id.toString();
+        boolean idPresentInRedis = jedis.exists(key);
+        if(idPresentInRedis) {
+            LOGGER.info("Deleting the old key");
+            jedis.del(key);
+        }
+
+        Optional<Restaurant> restaurantOptional = restaurantRepo.findById(id);
+        if(restaurantOptional.isPresent()) {
+            Restaurant oldRestaurant = restaurantOptional.get();
+            oldRestaurant.setCity(newRestaurant.getCity());
+            oldRestaurant.setEmail(newRestaurant.getEmail());
+            oldRestaurant.setName(newRestaurant.getName());
+            oldRestaurant.setPhone(newRestaurant.getPhone());
+            LOGGER.info("Updating the entity");
+            return restaurantRepo.save(oldRestaurant);
+        }else {
+            return new Restaurant();
+        }
     }
 }
